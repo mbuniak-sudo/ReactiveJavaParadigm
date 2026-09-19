766574472b98ad7fece95f8a19cb6c37031f00dd
mbuniak <mbuniak@griddynamics.com>
2026-09-19 21:42:03 +0200

    tests back

--- product-info-service/src/test/java/com/griddynamics/reactive/course/productinfoservice/controller/ProductInfoControllerTest.java
+++ product-info-service/src/test/java/com/griddynamics/reactive/course/productinfoservice/controller/ProductInfoControllerTest.java
@@ -1,13 +1,20 @@
 package com.griddynamics.reactive.course.productinfoservice.controller;
 
+import com.griddynamics.reactive.course.productinfoservice.domain.Product;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Duration;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,8 +25,22 @@ public class ProductInfoControllerTest {
     @Autowired
     WebTestClient webTestClient;
 
+
     @Test
     public void shouldReturnListOfProduct_WhenGetByNamesCall() {
-
+        webTestClient.mutate()
+                .responseTimeout(Duration.ofSeconds(15))
+                .build()
+                .get()
+                .uri(builder -> builder
+                        .path("/productInfoService/product/names/{productCode}")
+                        .build("101"))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(Product.class)
+                .consumeWith(response -> assertEquals(1,
+                        Objects.requireNonNull(response.getResponseBody()).size()));
     }
 }
